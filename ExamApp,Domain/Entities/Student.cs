086a58495bfa677f6e11d139b6b17cdc291f5ed4
Orkhan <aliyevorhan82@gmail.com>
2024-09-19 19:11:23 +0400

    add student model

---
+++ ExamApp,Domain/Entities/Student.cs
@@ -0,0 +1,10 @@
+﻿namespace ExamApp_Domain.Entities
+{
+    public class Student : BaseEntity
+    {
+        public uint StudentNumber { get; set; } // number(5,0)
+        public string Firstname { get; set; } = string.Empty; // varchar(30)
+        public string Lastname { get; set; } = string.Empty; // varchar(30)
+        public byte ClassNumber { get; set; } // number(2,0) 
+    }
+}
