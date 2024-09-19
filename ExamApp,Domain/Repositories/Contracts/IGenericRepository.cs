380d765e82ca5ec83dca49bbf9a5cf64cfeec4ee
Orkhan <aliyevorhan82@gmail.com>
2024-09-20 02:13:49 +0400

    add generic interface

---
+++ ExamApp,Domain/Repositories/Contracts/IGenericRepository.cs
@@ -0,0 +1,13 @@
+﻿using ExamApp_Domain.Entities;
+
+namespace ExamApp_Domain.Repositories.Contracts
+{
+    internal interface IGenericRepository<TEntity> where TEntity : BaseEntity, new()
+    {
+        Task CreateAsync(TEntity entity);
+        Task<IEnumerable<TEntity>> GetAllAsync();
+        Task<TEntity> GetByIdAsync(int id);
+        Task UpdateAsync(TEntity entity);
+        Task DeleteAsync(int id);
+    }
+}
