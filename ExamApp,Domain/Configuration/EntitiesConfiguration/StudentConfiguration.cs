b787465e12087ab45edc0e052ac7b7f733c845a4
Orkhan <aliyevorhan82@gmail.com>
2024-09-20 00:15:06 +0400

    add student configuration

---
+++ ExamApp,Domain/Configuration/EntitiesConfiguration/StudentConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using ExamApp_Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ExamApp_Domain.Configuration.EntitiesConfiguration
+{
+    internal class StudentConfiguration : IEntityTypeConfiguration<Student>
+    {
+        public void Configure(EntityTypeBuilder<Student> builder)
+        {
+            builder.ToTable("Student");
+
+            builder.Property(x => x.StudentNumber)
+                .IsRequired()
+                .HasColumnType("number(5,0)");
+
+            builder.Property(x => x.Firstname)
+                .IsRequired()
+                .HasMaxLength(30)
+                .HasColumnType("varchar(30)");
+
+            builder.Property(x => x.Lastname)
+                .IsRequired()
+                .HasMaxLength(30)
+                .HasColumnType("varchar(30)");
+
+            builder.Property(x => x.ClassNumber)
+                .IsRequired()
+                .HasColumnType("number(2,0)");
+
+        }
+    }
+}
