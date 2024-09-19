ded5bddd8686982c72c9e637361317f53db7c1d3
Orkhan <aliyevorhan82@gmail.com>
2024-09-20 03:48:51 +0400

    register domain services

--- ExamApp.Api/Program.cs
+++ ExamApp.Api/Program.cs
@@ -1,4 +1,6 @@
 
+using ExamApp_Domain;
+
 namespace ExamApp.Api
 {
     public class Program
@@ -6,17 +8,19 @@ namespace ExamApp.Api
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
+            
+            builder.Services.AddDomainRepositories();
+            builder.Services.AddDbContext(builder.Configuration);
+            
 
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+           
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
+            
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
