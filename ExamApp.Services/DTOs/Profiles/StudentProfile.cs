a2c5b80d79647c0a7355c3cad03e0133506877fe
Orkhan <aliyevorhan82@gmail.com>
2024-09-21 01:22:31 +0400

    add student profile

---
+++ ExamApp.Services/DTOs/Profiles/StudentProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using ExamApp.Domain.Entities;
+
+namespace ExamApp.Services.DTOs.Profiles
+{
+    internal class StudentProfile : Profile
+    {
+        public StudentProfile()
+        {
+            // в обе стороны так как имена полей идентичны
+            CreateMap<StudentDto, Student>().ReverseMap();
+        }
+    }
+}
