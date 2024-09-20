bf23c534c9f821d3e8d6f8d87b64466262e57e1b
Orkhan <aliyevorhan82@gmail.com>
2024-09-21 03:06:20 +0400

    fix rules for date field validation

--- ExamApp.Services/DTOs/DTOValidator/ExamDtoValidator.cs
+++ ExamApp.Services/DTOs/DTOValidator/ExamDtoValidator.cs
@@ -1,4 +1,5 @@
 ﻿using FluentValidation;
+using System.Globalization;
 
 namespace ExamApp.Services.DTOs.DTOValidator
 {
@@ -16,13 +17,24 @@ namespace ExamApp.Services.DTOs.DTOValidator
                 .WithMessage("StudentNumber must be between 1 and 99999.");
 
             RuleFor(x => x.Date)
-                .LessThanOrEqualTo(DateOnly.FromDateTime(DateTime.Now)) // считаем что экзамен уже был
-                .WithMessage("Date cannot be in the future.");
+                .Must(date =>
+                {
+                    if (string.IsNullOrWhiteSpace(date)) return false; // Проверка на пустоту
+                    return DateOnly.TryParse(date, out var parsedDate) &&
+                    parsedDate <= DateOnly.FromDateTime(DateTime.UtcNow);
+                })
+                .WithMessage("Date must be in the format yyyy-MM-dd or MM/dd/yyyy and must not be in the future.");
 
             RuleFor(x => x.Mark)
                 .Must(x => x > 0 && x < 6) // возможно максимальная оценка 5
                 .WithMessage("Mark must be between 1 and 5.");
         }
+
+        //private bool IsValidDate(string dateString)
+        //{
+        //    var formats = new[] { "yyyy-MM-dd", "MM/dd/yyyy", "dd/MM/yyyy" }; // добавьте нужные форматы
+        //    return DateTime.TryParseExact(dateString, formats, CultureInfo.InvariantCulture, DateTimeStyles.None, out _);
+        //}
     }
 
 }
--- ExamApp.Services/DTOs/ExamDto.cs
+++ ExamApp.Services/DTOs/ExamDto.cs
@@ -4,7 +4,7 @@
     {
         public required string SubjectCode { get; init; } = string.Empty; // char(3)
         public required int StudentNumber { get; init; }  // number(5,0)
-        public required DateOnly Date { get; init; }      // date
+        public required string Date { get; init; }      // date as string иначе придется лепить правила для конвертации и мне лениво
         public required byte Mark { get; init; }          // number(1,0)
     }
 }
