1bdd57f332565e4d6c9f124cba60766472acef6f
Orkhan <aliyevorhan82@gmail.com>
2024-09-20 22:11:01 +0400

    add extention class

---
+++ ExamApp.Services/ServiceCollectionExtention.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using FluentValidation.AspNetCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace ExamApp.Services
+{
+    public static class ServiceCollectionExtention
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly())
+                .AddFluentValidationAutoValidation(); // автовалидация в контроллере
+            // TODO: register services
+
+            return services;
+        }
+
+    }
+}
