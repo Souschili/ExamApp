20c2b5369a2b4e8fa03c1ceb7ab28b86fb098fa5
Orkhan <aliyevorhan82@gmail.com>
2024-09-21 01:20:47 +0400

    add subject profile

---
+++ ExamApp.Services/DTOs/Profiles/SubjectProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using ExamApp.Domain.Entities;
+
+namespace ExamApp.Services.DTOs.Profiles
+{
+    internal class SubjectProfile : Profile
+    {
+        public SubjectProfile()
+        {
+            // в обе стороны так как имена полей идентичны
+            CreateMap<SubjectDto, Subject>().ReverseMap();
+        }
+    }
+}
