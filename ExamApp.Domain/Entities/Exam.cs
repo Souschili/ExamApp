4096b7bd9566e6f4db8a87b9b8bca97db285c371
Orkhan <aliyevorhan82@gmail.com>
2024-09-20 22:50:39 +0400

    change ushort to int for Student Number field

--- ExamApp.Domain/Entities/Exam.cs
+++ ExamApp.Domain/Entities/Exam.cs
@@ -3,7 +3,7 @@
     public sealed class Exam : BaseEntity
     {
         public string SubjectCode { get; set; } = string.Empty; // char(3)
-        public ushort StudentNumber { get; set; }  // number(5,0)
+        public int StudentNumber { get; set; }  // number(5,0)
         public DateOnly Date { get; set; }      // date
         public byte Mark { get; set; }          // number(1,0)
     }
--- ExamApp.Domain/Entities/Student.cs
+++ ExamApp.Domain/Entities/Student.cs
@@ -2,7 +2,7 @@
 {
     public sealed class Student : BaseEntity
     {
-        public ushort StudentNumber { get; set; } // number(5,0)
+        public int StudentNumber { get; set; } // number(5,0)
         public string Firstname { get; set; } = string.Empty; // varchar(30)
         public string Lastname { get; set; } = string.Empty; // varchar(30)
         public byte ClassNumber { get; set; } // number(2,0) 
