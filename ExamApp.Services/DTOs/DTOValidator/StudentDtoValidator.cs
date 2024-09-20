352e3081916c11b8c1e9c8ff5ed7c75f6d7c53bb
Orkhan <aliyevorhan82@gmail.com>
2024-09-20 22:26:18 +0400

    add validation rules for student

---
+++ ExamApp.Services/DTOs/DTOValidator/StudentDtoValidator.cs
@@ -0,0 +1,29 @@
+﻿using FluentValidation;
+
+namespace ExamApp.Services.DTOs.DTOValidator
+{
+    public class StudentDtoValidator : AbstractValidator<StudentDto>
+    {
+        public StudentDtoValidator()
+        {
+            RuleFor(x => x.StudentNumber)
+                .Must(x => x > 0 && x < 10000)
+                .WithMessage("StudentNumber must be between 1 and 99999.");
+
+            RuleFor(x => x.Firstname)
+                .NotEmpty()
+                .Length(1, 30)
+                .WithMessage("Firstname cannot be empty and must be up to 30 characters long.");
+
+            RuleFor(x => x.Lastname)
+                .NotEmpty()
+                .Length(1, 30)
+                .WithMessage("Lastname cannot be empty and must be up to 30 characters long.");
+
+            RuleFor(x => x.ClassNumber)
+                .Must(x => x > 0 && x <= 11)
+                .WithMessage("ClassNumber must be between 1 and 11.");
+        }
+    }
+
+}
