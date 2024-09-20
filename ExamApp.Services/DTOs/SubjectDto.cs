b5fb247dd0889fd58746fb125d34e3ab7bfc5df5
Orkhan <aliyevorhan82@gmail.com>
2024-09-20 20:42:38 +0400

    add DTO model for subkect

---
+++ ExamApp.Services/DTOs/SubjectDto.cs
@@ -0,0 +1,11 @@
+﻿namespace ExamApp.Services.DTOs
+{
+    public record class SubjectDto
+    {
+        public required string SubjectCode { get; init; } = string.Empty; // char (3)
+        public required string SubjectName { get; init; } = string.Empty; // varchar(30)
+        public required byte ClassNumber { get; init; }  // number(2,0)
+        public required string TeacherFirstName { get; init; } = string.Empty; // varchar(20)
+        public required string TeacherLastName { get; init; } = string.Empty;  // varchar(20)
+    }
+}
