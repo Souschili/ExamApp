5df931f220b6deebfc4329e3f78e23116a1426d9
Orkhan <aliyevorhan82@gmail.com>
2024-09-20 21:28:04 +0400

    add exam dto

---
+++ ExamApp.Services/DTOs/ExamDto.cs
@@ -0,0 +1,10 @@
+﻿namespace ExamApp.Services.DTOs
+{
+    internal class ExamDto
+    {
+        public required string SubjectCode { get; init; } = string.Empty; // char(3)
+        public required ushort StudentNumber { get; init; }  // number(5,0)
+        public required DateOnly Date { get; init; }      // date
+        public required byte Mark { get; init; }          // number(1,0)
+    }
+}
