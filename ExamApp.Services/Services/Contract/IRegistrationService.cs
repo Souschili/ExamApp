34434903581d5f86214390ca5a6fbaeb678663ed
Orkhan <aliyevorhan82@gmail.com>
2024-09-20 23:22:34 +0400

    add folders and interface

---
+++ ExamApp.Services/Services/Contract/IRegistrationService.cs
@@ -0,0 +1,11 @@
+﻿using ExamApp.Services.DTOs;
+
+namespace ExamApp.Services.Services.Contract
+{
+    public interface IRegistrationService
+    {
+        Task SubjectRegistration(SubjectDto subjectDto);
+        Task StudentRegistration(StudentDto studentDto);
+        Task ExamRegistration(ExamDto examDto);
+    }
+}
