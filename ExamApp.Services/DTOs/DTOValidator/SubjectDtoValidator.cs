f4ab29a5ba16523e685a29b23c19b9baf05069b3
Orkhan <aliyevorhan82@gmail.com>
2024-09-20 21:18:57 +0400

    add rules for subject dto

---
+++ ExamApp.Services/DTOs/DTOValidator/SubjectDtoValidator.cs
@@ -0,0 +1,41 @@
+﻿using FluentValidation;
+
+namespace ExamApp.Services.DTOs.DTOValidator
+{
+    public class SubjectDtoValidator : AbstractValidator<SubjectDto>
+    {
+        public SubjectDtoValidator()
+        {
+            // SubjectCode: exactly 3 characters
+            RuleFor(x => x.SubjectCode)
+                .NotEmpty()
+                .Length(3)
+                .WithMessage("SubjectCode must consist of exactly 3 characters.");
+
+            // SubjectName: required, max length 30 characters
+            RuleFor(x => x.SubjectName)
+                .NotEmpty()
+                .MaximumLength(30)
+                .WithMessage("SubjectName cannot be empty and must not exceed 30 characters.");
+
+            // ClassNumber: byte (0-255), rule for valid range 1 to 12
+            RuleFor(x => x.ClassNumber)
+                .Must(x => x > 0 && x < 12)
+                .WithMessage("ClassNumber must be between 1 and 11.");
+
+
+            // TeacherFirstName: required, max length 20 characters
+            RuleFor(x => x.TeacherFirstName)
+                .NotEmpty()
+                .MaximumLength(20)
+                .WithMessage("TeacherFirstName cannot be empty and must not exceed 20 characters.");
+
+            // TeacherLastName: required, max length 20 characters
+            RuleFor(x => x.TeacherLastName)
+                .NotEmpty()
+                .MaximumLength(20)
+                .WithMessage("TeacherLastName cannot be empty and must not exceed 20 characters.");
+
+        }
+    }
+}
