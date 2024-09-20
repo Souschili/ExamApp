051aa0073f3dbeab7e87608d898c7418460689cf
Orkhan <aliyevorhan82@gmail.com>
2024-09-20 21:25:30 +0400

    add student dto

---
+++ ExamApp.Services/DTOs/StudentDto.cs
@@ -0,0 +1,10 @@
+﻿namespace ExamApp.Services.DTOs
+{
+    public class StudentDto
+    {
+        public required ushort StudentNumber { get; init; } // number(5,0)
+        public required string Firstname { get; init; } = string.Empty; // varchar(30)
+        public required string Lastname { get; init; } = string.Empty; // varchar(30)
+        public required byte ClassNumber { get; init; } // number(2,0) 
+    }
+}
