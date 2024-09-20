c0e7d5fceba514a92a05e3c34b1c2b3091231ec8
Orkhan <aliyevorhan82@gmail.com>
2024-09-21 03:37:18 +0400

    fix rules for mapping

--- ExamApp.Services/DTOs/Profiles/ExamProfile.cs
+++ ExamApp.Services/DTOs/Profiles/ExamProfile.cs
@@ -8,7 +8,15 @@ namespace ExamApp.Services.DTOs.Profiles
         public ExamProfile()
         {
             // в обе стороны так как имена полей идентичны
-            CreateMap<ExamDto, Exam>().ReverseMap();
+            //CreateMap<ExamDto, Exam>().ReverseMap();
+
+            // тут правила чуток другие изза того что в дто строка,
+            // а надо преобразовать в DateOnly и обратно
+            CreateMap<ExamDto, Exam>()
+                .ForMember(dest => dest.Date, cfg => cfg.MapFrom(src => DateOnly.Parse(src.Date)))
+                .ReverseMap()
+                // можно сменить формат строкового преобразования 
+                .ForMember(dest => dest.Date, cfg => cfg.MapFrom(src => src.Date.ToString("yyyy-MM-dd")));
         }
     }
 }
