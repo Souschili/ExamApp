4445f9f16d007c381971b672692ced7794c6919f
Orkhan <aliyevorhan82@gmail.com>
2024-09-20 22:43:58 +0400

    add wxan validator , fix access type ,fix studnum field type

---
+++ ExamApp.Services/DTOs/DTOValidator/ExamDtoValidator.cs
@@ -0,0 +1,28 @@
+﻿using FluentValidation;
+
+namespace ExamApp.Services.DTOs.DTOValidator
+{
+    public class ExamDtoValidator : AbstractValidator<ExamDto>
+    {
+        public ExamDtoValidator()
+        {
+            RuleFor(x => x.SubjectCode)
+                .NotEmpty()
+                .Length(3)
+                .WithMessage("SubjectCode must be exactly 3 characters long.");
+
+            RuleFor(x => x.StudentNumber)
+                .Must(x => x > 1 && x < 100000)
+                .WithMessage("StudentNumber must be between 1 and 99999.");
+
+            RuleFor(x => x.Date)
+                .LessThanOrEqualTo(DateOnly.FromDateTime(DateTime.Now)) // считаем что экзамен уже был
+                .WithMessage("Date cannot be in the future.");
+
+            RuleFor(x => x.Mark)
+                .Must(x => x > 0 && x < 6) // возможно максимальная оценка 5
+                .WithMessage("Mark must be between 1 and 5.");
+        }
+    }
+
+}
--- ExamApp.Services/DTOs/ExamDto.cs
+++ ExamApp.Services/DTOs/ExamDto.cs
@@ -1,9 +1,9 @@
 ﻿namespace ExamApp.Services.DTOs
 {
-    internal class ExamDto
+    public class ExamDto
     {
         public required string SubjectCode { get; init; } = string.Empty; // char(3)
-        public required ushort StudentNumber { get; init; }  // number(5,0)
+        public required int StudentNumber { get; init; }  // number(5,0)
         public required DateOnly Date { get; init; }      // date
         public required byte Mark { get; init; }          // number(1,0)
     }
