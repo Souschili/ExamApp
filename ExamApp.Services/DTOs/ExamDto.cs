a5f1b40d3b78d878f037c9e407b0727f100d4dc2
Orkhan <aliyevorhan82@gmail.com>
2024-09-21 04:50:13 +0400

    add comment

--- ExamApp.Services/DTOs/ExamDto.cs
+++ ExamApp.Services/DTOs/ExamDto.cs
@@ -5,6 +5,6 @@
         public required string SubjectCode { get; init; } = string.Empty;
         public required int StudentNumber { get; init; }
         public required string Date { get; init; }      // date as string иначе придется лепить правила для конвертации и мне лениво
-        public required int Mark { get; init; }
+        public required int Mark { get; init; }         // чтобы ловить негативные значения при валидации в сущности это поле байт
     }
 }
