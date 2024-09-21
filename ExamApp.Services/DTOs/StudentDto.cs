e973bf537cd8fa6b0dfbe81bc20812e27d64c9da
Orkhan <aliyevorhan82@gmail.com>
2024-09-21 05:08:19 +0400

    change byte to int in studentDto

--- ExamApp.Services/DTOs/StudentDto.cs
+++ ExamApp.Services/DTOs/StudentDto.cs
@@ -2,9 +2,9 @@
 {
     public class StudentDto
     {
-        public required int StudentNumber { get; init; } 
-        public required string Firstname { get; init; } = string.Empty; 
-        public required string Lastname { get; init; } = string.Empty; 
-        public required byte ClassNumber { get; init; }  
+        public required int StudentNumber { get; init; }
+        public required string Firstname { get; init; } = string.Empty;
+        public required string Lastname { get; init; } = string.Empty;
+        public required int ClassNumber { get; init; }   // чтобы ловить негативные значения при валидации в сущности это поле байт
     }
 }
