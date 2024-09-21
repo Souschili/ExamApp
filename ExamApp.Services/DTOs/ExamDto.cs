d7a709ab3e30249531b9aa876854705ced383415
Orkhan <aliyevorhan82@gmail.com>
2024-09-21 04:46:17 +0400

    chanfe byte to int , reason to catch negative value in validation

--- ExamApp.Services/DTOs/ExamDto.cs
+++ ExamApp.Services/DTOs/ExamDto.cs
@@ -5,6 +5,6 @@
         public required string SubjectCode { get; init; } = string.Empty;
         public required int StudentNumber { get; init; }
         public required string Date { get; init; }      // date as string иначе придется лепить правила для конвертации и мне лениво
-        public required byte Mark { get; init; }
+        public required int Mark { get; init; }
     }
 }
