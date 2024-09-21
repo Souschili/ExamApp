9cf17c678eb06f51c51fa5564d1c2a9e105a6396
Orkhan <aliyevorhan82@gmail.com>
2024-09-21 15:26:46 +0400

    add global exception handler

---
+++ ExamApp.Api/ExceptionHandlers/GlobalExceptionHandler.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+
+namespace ExamApp.Api.ExceptionHandlers
+{
+    internal class GlobalExceptionHandler : IExceptionHandler
+    {
+        public ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
